83f40dcac016db2be26fc18c69bd84394246eb47
中村 悠人 <yewtoe@nakamura-no-iMac.local>
2017-12-12 04:38:17 +0900

    Commit for the first time

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cpractice1
+//
+//  Created by 中村 悠人 on 2017/03/03.
+//  Copyright © 2017年 中村　悠人. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
